3224121594290191b15e01c0febe37fb10a73d3f
Geetika1214 <145048481+Geetika1214@users.noreply.github.com>
2023-09-21 22:38:29 +0530

    Add files via upload
    Task 1 : A simple Calculator using  c ++ which performs basic calculations

---
+++ simple calculator.cpp
@@ -0,0 +1,47 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	cout<<"A simple Calculator "<<endl;
+	while(true)
+	{
+		double a ,b;
+		char op;
+		  cout<<"enter the operator:"<<endl;
+		  cin>>op;
+		  cout<<"enter the number 1:"<<endl;
+		  cin>>a;
+		    cout<<"enter the number 2:"<<endl;
+		  cin>>b;
+		  
+		  switch(op)
+		  {
+		  
+		  case '+':
+		  	cout<<a<<" "<<op<<" "<<b<<"="<<a+b<<endl;
+		  	break;
+		  	
+		  	 case '-':
+		  	cout<<a<<" "<<op<<" "<<b<<"="<<a-b<<endl;
+		  	break;
+		  	
+		  	 case '*':
+		  	cout<<a<<" "<<op<<" "<<b<<"="<<a*b<<endl;
+		  	break;
+		  	
+		  	 case '/':
+		  	 	if(b==0){
+		  	 		cout<<"dividing by zero"<<endl;
+				   }
+				   else{
+				   	cout<<a<<" "<<op<<" "<<b<<"="<<a/b<<endl;
+				   }
+		  	       break;
+		    	default:
+		  		cout<<"invalid operatr value"<<endl;
+		  }
+		  
+		  }
+		    
+	return 0;
+}
